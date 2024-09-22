e9d7dcf8ef91a8f5d3e27fa4f165e9596eb0aff0
Aman <120547566+Aman-008@users.noreply.github.com>
2024-09-22 18:33:32 +0530

    commited

--- e-commerce/frontend/src/Pages/LoinSignUp.jsx
+++ e-commerce/frontend/src/Pages/LoinSignUp.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const LoinSignUp = () => {
   return (
-    <div>
-      
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the term of use & privacy policy.</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoinSignUp
+export default LoinSignUp;
